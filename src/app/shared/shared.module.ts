db8e1ca0f275178b4af00957200f1510f9797a6e
Jack Borrie <lunarinlitum@hotmail.com>
2022-02-11 15:13:58 +1300

    Corrected mistakes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,10 @@
 import { NgModule }     from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FlowCanvasComponent } from './components/flow-canvas/flow-canvas.component';
 
 @NgModule ({
     declarations: [
-        FlowCanvasComponent
     ],
     exports: [
-        FlowCanvasComponent
     ],
     imports: [
         CommonModule
