e078d442e1e0716e30a631f17ecab8db54a4edd2
inlitum <lunarinlitum@hotmail.com>
2022-09-03 14:15:18 +1200

    Initial commit of the sloth dropdown component

--- src/app/components/sloth-navbar/sloth-navbar.component.ts
+++ src/app/components/sloth-navbar/sloth-navbar.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from "rxjs";
+import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
-  selector: 'sloth-navbar',
-  templateUrl: './sloth-navbar.component.html',
-  styleUrls: ['./sloth-navbar.component.scss']
+    selector: 'sloth-navbar',
+    templateUrl: './sloth-navbar.component.html',
+    styleUrls: ['./sloth-navbar.component.scss']
 })
-export class SlothNavbarComponent implements OnInit {
+export class SlothNavbarComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+    private _subscriptions: Subscription = new Subscription();
 
-  ngOnInit(): void {
-  }
+    public url: string | null = null;
+
+    constructor(private _router: Router) {
+    }
+
+    ngOnInit(): void {
+        let routerSub = this._router.events.subscribe(event => {
+            if (event instanceof NavigationEnd) {
+                this.url = event.url;
+            }
+        })
+
+        this._subscriptions.add(routerSub);
+    }
+
+    ngOnDestroy() {
+        this._subscriptions.unsubscribe();
+    }
 
 }
